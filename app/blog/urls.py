53865c59fda9686643e705969cec5bef962e9d22
Ryanden <lockstom@gmail.com>
2018-06-01 16:08:59 +0900

    config.urls에서 r''(모든 연결)을 blog.urls에서 처리하도록 include blog.urls의 r'^$'(루트 URL, /)에 blog.views의 post_list view function을연결

---
+++ app/blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from .views import post_list
+
+# Browser -> request -> Django -> config.urls -> blog.urls -> r'^$'에 매칭 ->
+# df post_list -> return 값을 다시 Browser
+urlpatterns = [
+    # url의 첫번째 인자 URL정규표현식
+    # url의 두번째 인자 view function
+    #   view_function
+    #       -> request를 받아서 response를 돌려주는 함수
+
+    # 함수를 전달함
+    url(r'^$', post_list),
+]
