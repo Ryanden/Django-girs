ec7d9664794302c2f91b86fefe7f795107914fbf
Ryanden <lockstom@gmail.com>
2018-06-05 12:03:45 +0900

    post_list에서 post_detail로 가는 링크 생성

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -4,12 +4,12 @@ from .views import post_list, post_detail
 # Browser -> request -> Django -> config.urls -> blog.urls -> r'^$'에 매칭 ->
 # df post_list -> return 값을 다시 Browser
 urlpatterns = [
-    # url의 첫번째 인자 URL정규표현식
-    # url의 두번째 인자 view function
+    # url 의 첫번째 인자 URL 정규표현식
+    # url 의 두번째 인자 view function
     #   view_function
-    #       -> request를 받아서 response를 돌려주는 함수
+    #       -> request 를 받아서 response 를 돌려주는 함수
 
     # 함수를 전달함
-    url(r'^$', post_list),
-    url(r'^(\d+)', post_detail),
+    url(r'^$', post_list, name='post-list'),
+    url(r'^(\d+)/', post_detail, name='post-detail'),
 ]
